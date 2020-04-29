c427cb2c5e2f5beb9c7d070335d528790427bd9f
patrick-lainesse <patrick.lainesse@umontreal.ca>
2020-04-29 09:19:57 -0400

    Nearing optimized solution, but still a problem with longer arrays.

--- src/Solution.java
+++ src/Solution.java
@@ -6,11 +6,13 @@ public class Solution {
         //int[] nums1 = {1,1,2};
         //int[] nums2 = {0,0,1,1,1,2,2,3,3,4};
         int newLenght = 1;
-        int last = 1;
+        int last = 0;
+        int higher = nums[0];
 
         for(int i=1;i<nums.length; i++){
-            if(nums[i] > nums[last]) {
+            if(nums[i] > higher) {
                 nums[last+1] = nums[i];
+                higher = nums[i];
                 last = i;
                 newLenght++;
             }

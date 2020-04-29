9cfc1af229b8c46e87c9187fd8234590d3d7b865
patrick-lainesse <patrick.lainesse@umontreal.ca>
2020-04-29 15:18:38 -0400

    Problem solved, but is not the optimal solution compared to distribution charts.

--- src/Solution.java
+++ src/Solution.java
@@ -1,28 +1,25 @@
-import java.util.Arrays;
-
 public class Solution {
     public static int removeDuplicates(int[] nums) {
 
-        //int[] nums1 = {1,1,2};
-        //int[] nums2 = {0,0,1,1,1,2,2,3,3,4};
-        int newLenght = 1;
-        int last = 0;
-        int higher = nums[0];
+        int newLength = 0;
+        int higher = Integer.MIN_VALUE;
 
-        for(int i=1;i<nums.length; i++){
-            if(nums[i] > higher) {
-                nums[last+1] = nums[i];
-                higher = nums[i];
-                last = i;
-                newLenght++;
+        for(int i=0;i<nums.length; i++){
+            for(int j=i; j<nums.length; j++) {
+                if(nums[j] > higher) {
+                    nums[i] = nums[j];
+                    higher = nums[j];
+                    newLength++;
+                    break;
+                }
             }
         }
 
-        for (int i : nums) {
+        /*for (int i : nums) {
             System.out.printf("%d", i);
-        }
+        }*/
 
-        return newLenght;
+        return newLength;
     }
 
     public static void main(String[] args) {
@@ -31,7 +28,13 @@ public class Solution {
         int[] nums2 = {0,0,1,1,1,2,2,3,3,4};
 
         System.out.println("\nRemove nums1: " + removeDuplicates(nums1));
-        System.out.println("Remove nums2: " + removeDuplicates(nums2));
+        for (int i : nums1) {
+            System.out.printf("%d", i);
+        }
+        System.out.println("\nRemove nums2: " + removeDuplicates(nums2));
+        for (int i : nums2) {
+            System.out.printf("%d", i);
+        }
 
     }
 }

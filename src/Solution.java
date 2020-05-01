7c7bbe2c51ddec7e0d57aae8a5d3240ca42b9fe9
patrick-lainesse <patrick.lainesse@umontreal.ca>
2020-05-01 00:49:58 -0400

    Succeeded with HashMap, but now I need to make sure a same index can't be added twice in the array. I can't remove a value from a HashMap while iterating over it. Maybe use an ArrayList?

--- src/Solution.java
+++ src/Solution.java
@@ -1,21 +1,30 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Solution {
     /* Given an array of integers, return indices of the two numbers such that they add up to a specific target.
     You may assume that each input would have exactly one solution, and you may not use the same element twice. */
     public static int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>(nums.length);
 
         for(int i=0; i<nums.length; i++) {
-            for(int j=0; j<nums.length; j++) {
-                if(nums[i] + nums[j] == target) {
-                    if(i>j) {
-                        result[0] = j;
-                        result[1] = i;
-                    } else {
-                        result[0] = i;
-                        result[1] = j;
-                    }
+            map.put(i, nums[i]);
+        }
+
+        for(int i : map.keySet()) {
+            int num1 = map.get(i);
+            int num2 = target - num1;
+
+            // problem here, can return the same key as num1. see:
+            // https://stackoverflow.com/questions/1383797/java-hashmap-how-to-get-key-from-value
+            if(map.containsValue(num2)) {
+                if(i < num2) {
+                    result[0] = map.get(num1);
+                    result[1] = map.get(num2);
+                } else {
+                    result[0] = map.get(num2);
+                    result[1] = map.get(num1);
                 }
             }
         }
@@ -37,20 +46,15 @@ public class Solution {
                 }
             }
         }
-
-        /*for (int i : nums) {
-            System.out.printf("%d", i);
-        }*/
-
         return newLength;
     }
 
     public static void main(String[] args) {
 
         int[] nums = {2, 7, 11, 15};
-        //for(int i=0;i<2;i++) {
-            System.out.println(Arrays.toString(twoSum(nums, 9)));
-        //}
+        int[] nums2 = {3, 3};
+        System.out.println(Arrays.toString(twoSum(nums, 9)));
+        System.out.println(Arrays.toString(twoSum(nums2, 6)));
 
     }
 }
